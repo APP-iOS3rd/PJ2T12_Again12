8a2f2bf6472aaecb04a7d7a3a6a9a63ff2dab178
burst-cannon <qlrmr111@naver.com>
2023-12-08 16:17:37 +0900

    Feat: 전체적인 UI 생성, search칸 생성, 하고싶은일/해야하는일 toggle 임시 생성

--- PJ2T12_Again12/PJ2T12_Again12/Views/HistoryView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/HistoryView.swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct HistoryView: View {
+@State private var searchTitle: String = ""
+@State private var selectedSegment = 0
+    
+    struct Itemo: Identifiable {
+            var id = UUID()
+            var nameodd: String
+        }
+    
+    struct Iteme: Identifiable {
+            var id = UUID()
+            var nameeven: String
+        }
+        
+    let itemodd: [Itemo] = [
+        Itemo(nameodd: "항목 1"),
+        Itemo(nameodd: "항목 3"),
+        Itemo(nameodd: "항목 5"),
+        Itemo(nameodd: "항목 7"),
+        Itemo(nameodd: "항목 9"),
+        Itemo(nameodd: "항목 11")
+    ]
+    
+    let itemeven: [Iteme] = [
+        Iteme(nameeven: "항목 2"),
+        Iteme(nameeven: "항목 4"),
+        Iteme(nameeven: "항목 6"),
+        Iteme(nameeven: "항목 8"),
+        Iteme(nameeven: "항목 10"),
+        Iteme(nameeven: "항목 12")
+    ]
+    
+//    let todoList: [Todo]
+//    let haveToList: [WantTodo]
+    
     var body: some View {
-        Text("History View")
+        VStack {
+            VStack {
+                Text("전체 일정 보기")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                
+                Picker("Select Segment", selection: $selectedSegment) {
+                    Text("해야 하는 일").tag(0)
+                    Text("하고 싶은 일").tag(1)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+            }
+            
+            NavigationView {
+                if selectedSegment == 0 {
+                    List(itemodd) { item in
+                        Text(item.nameodd)
+                    }
+                    .searchable(text: $searchTitle)
+                } else {
+                    List(itemeven) { item in
+                        Text(item.nameeven)
+                    }
+                    .searchable(text: $searchTitle)
+                }
+            }
+        }
     }
 }
-
 #Preview {
     HistoryView()
 }
