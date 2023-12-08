d490b7fbe902a634bdd544dade5563006a1761ce
Im-loki <al4126@naver.com>
2023-12-08 16:50:37 +0900

    메달 LazyVGrid 생성

--- PJ2T12_Again12/PJ2T12_Again12/Views/StatusView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/StatusView.swift
@@ -10,11 +10,18 @@ import Charts
 
 struct StatusView: View {
     struct ToyShape: Identifiable {
-        @State var month: [String] = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
+        var month: [String] = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
         var color: String
         var count: Double
         var id = UUID()
     }
+    //목록을 1부터 1000까지 만듬
+    let data = Array(1...1000).map { "목록 \($0)"}
+    
+    //화면을 그리드형식으로 꽉채워줌
+    let columns = [
+        GridItem(.adaptive(minimum: 100))
+    ]
     
     var stackedBarData: [ToyShape] = [
         // count는 높이
@@ -45,10 +52,19 @@ struct StatusView: View {
             .chartForegroundStyleScale([
                 "Grey": .gray, "Pink": .pink, "Green": .green
             ])
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Spacer()
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 20) {
+                        ForEach(data, id: \.self) {i in
+                            Text(i)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
         }
     }
-}
+
 
 #Preview {
     StatusView()
