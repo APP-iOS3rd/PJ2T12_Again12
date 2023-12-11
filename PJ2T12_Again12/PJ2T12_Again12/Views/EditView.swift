c45400444b58e05b174277f8bd68b3bfd4545952
wonny1012 <rnjswldnjs1012@gmail.com>
2023-12-11 17:27:55 +0900

    Feat: 사진첩접근권한구현

--- PJ2T12_Again12/PJ2T12_Again12/Views/EditView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/EditView.swift
@@ -7,83 +7,92 @@
 
 import SwiftUI
 import Foundation
+import Photos
 
 struct EditView: View {
+    @StateObject private var viewModel = EditViewModel()
     @State private var openPhoto = false
     @State private var image = UIImage()
     @State private var userText: String = ""
+    @State private var checkSave = false
     
     var body: some View {
         NavigationView {
-                        ScrollView {
-            VStack {
-                HStack(spacing: 20) {
-                    Image(systemName: "airplane")
-                        .font(.title2)
-                    Text("아침 일찍 일어나기")
-                        .font(.system(size: 25))
-                        .bold()
-                }
-                .frame(width: 340, height: 60)
-                HStack() {
-                    Button(action: {
-                        self.openPhoto = true
-                    }) {
-                        ZStack() {
-                            RoundedRectangle(cornerRadius: 12)
-                                .frame(width: 90, height: 90)
-                                .foregroundColor(Color(red: 0xD9 / 255.0, green: 0xD9 / 255.0, blue: 0xD9 / 255.0))
-                            Image(systemName: "camera.circle")
-                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                                .foregroundColor(.white)
+            ScrollView {
+                VStack {
+                    HStack(spacing: 20) {
+                        Image(systemName: "airplane")
+                            .font(.title2)
+                        Text("아침 일찍 일어나기")
+                            .font(.system(size: 25))
+                            .bold()
+                    }
+                    .frame(width: 340, height: 60)
+                    HStack {
+                        Button(action: {
+                            viewModel.checkAlbumPermission()
+                            if viewModel.albumPermissionGranted {
+                                self.openPhoto = true  
+                            }
+                        }) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .frame(width: 90, height: 90)
+                                    .foregroundColor(Color(red: 0xD9 / 255.0, green: 0xD9 / 255.0, blue: 0xD9 / 255.0))
+                                Image(systemName: "camera.circle")
+                                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                                    .foregroundColor(.white)
+                            }
+                            .padding()
                         }
-                        .padding()
+                        .sheet(isPresented: $openPhoto) {
+                            ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+                        }
+                        Spacer()
                     }
-                    .sheet(isPresented: $openPhoto) {
-                        ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+                    .padding()
+                    
+                    if image.size != CGSize.zero {
+                        Image(uiImage: image)
+                            .resizable()
+                            .frame(width: 320, height: 320)
+                            .scaledToFit()
                     }
+                    
+                    TextEditor(text: $userText)
+                        .frame(width: 320, height: 250)
+                        .lineSpacing(8)
+                        .keyboardType(.default)
+                        .border(Color.gray)
+                        .onTapGesture {
+                        }
+                        .overlay(
+                            Text("할 일을 마치며 느낀점을 적어주세요")
+                                .foregroundColor(.gray)
+                                .padding(EdgeInsets(top: 8, leading: 4, bottom: 0, trailing: 4))
+                                .opacity(userText.isEmpty ? 1 : 0)
+                        )
                     Spacer()
+                    
+                    Button(action: {
+                     checkSave = true
+                    }) {
+                        Text("달성 완료")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 170, height: 50)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
                 }
                 .padding()
-                
-                if image.size != CGSize.zero {
-                    Image(uiImage: image)
-                        .resizable()
-                        .frame(width: 320, height: 320)
-                        .scaledToFit()
                 }
-                
-                TextEditor(text: $userText)
-                    .frame(width: 320, height: 250)
-                    .lineSpacing(8)
-                    .keyboardType(.default)
-                    .border(Color.gray)
-                    .onTapGesture {
-                    }
-                    .overlay(
-                        Text("할 일을 마치며 느낀점을 적어주세요")
-                            .foregroundColor(.gray)
-                            .padding(EdgeInsets(top: 8, leading: 4, bottom: 0, trailing: 4))
-                            .opacity(userText.isEmpty ? 1 : 0)
-                    )
-                Spacer()
-                Button(action: {
-                }) {
-                    Text("달성 완료")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 170, height: 50)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }
-            }
-            .padding()
             .onTapGesture {
                 hideKeyboard()
             }
         }
-    }
+        
     }
 }
 
@@ -111,6 +120,7 @@ struct ImagePicker: UIViewControllerRepresentable {
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
+    
     
     final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
         
