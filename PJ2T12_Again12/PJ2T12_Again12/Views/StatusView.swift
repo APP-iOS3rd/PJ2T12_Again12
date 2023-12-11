649d9331c4e380ab85058a732add7a3ac6ce5362
Im-loki <al4126@naver.com>
2023-12-12 01:02:46 +0900

    Design: 막대그래프 데이터 수정과 가로스크롤 기능 부여

--- PJ2T12_Again12/PJ2T12_Again12/Views/StatusView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/StatusView.swift
@@ -4,64 +4,66 @@
 //
 //  Created by KHJ on 2023/12/07.
 //
-
+import Foundation
 import SwiftUI
 import Charts
 
 struct StatusView: View {
-    struct ToyShape: Identifiable {
-        var month: [String] = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
-        var color: String
-        var count: Double
-        var id = UUID()
-    }
-    //목록을 1부터 1000까지 만듬
-    let data = Array(1...1000).map { "목록 \($0)"}
-    
-    //화면을 그리드형식으로 꽉채워줌
+    let Data = [
+        (doType: "todo", data: ViewMonth.todoMonth),
+        (doType: "wantTodo", data: ViewMonth.wantTodoMonth),
+    ]
+
     let columns = [
-        GridItem(.adaptive(minimum: 100))
-    ]
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
     
-    var stackedBarData: [ToyShape] = [
-        // count는 높이
-        .init(color: "Grey", count: 2),
-        .init(color: "Grey", count: 0),
-        .init(color: "Grey", count: 1),
-        .init(color: "Pink", count: 1),
-        .init(color: "Pink", count: 1),
-        .init(color: "Pink", count: 1),
-        .init(color: "Green", count: 1),
-        .init(color: "Green", count: 2),
-        .init(color: "Green", count: 0),
-        
-    ]
     var body: some View {
         VStack {
-            Chart {
-                ForEach(stackedBarData) { shape in
-                    BarMark(
-                        x: .value("Shape Type", shape.month[0])
-                        ,
-                        y: .value("Total Count", shape.count)
+            Text("목표 달성도")
+                .font(.largeTitle)
+                .bold()
+            
+                    Chart {
+                        ForEach(Data, id: \.doType) { element in
+                            ForEach(element.data, id: \.date) {
+                                
+                                BarMark(x: .value("Month", $0.date),
+                                        y: .value("Count", $0.count))
+                                .foregroundStyle(.blue)
+                                if element.doType == "todo" {
+                                    BarMark(x: .value("Month", $0.date),
+                                            y: .value("Count", $0.count - 2))
+                                    .foregroundStyle(.gray)
+                                }
+                                
+                            }
                         
-                    )
-                    .foregroundStyle(by: .value("Shape Color", shape.color))
-                }
-            }
-            .chartForegroundStyleScale([
-                "Grey": .gray, "Pink": .pink, "Green": .green
-            ])
-            Spacer()
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(data, id: \.self) {i in
-                            Text(i)
+                            .position(by: .value("doType", element.doType))
+                            
                         }
                     }
-                    .padding(.horizontal)
+//                    .chartForegroundStyleScale([
+//                        "todo": .pink,
+//                        "wantTodo": .blue,
+//                        "notdo": .gray
+//                    ])
+                }
+                
+            
+        
+            
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(0..<6) { index in
+                    Text("Cell \(index)")
+                        .font(.system(size: 20))
+                        .frame(width: 100, height: 100)
+                        .background(Color.gray)
                 }
             }
+            .padding()
         }
     }
 
@@ -69,3 +71,9 @@ struct StatusView: View {
 #Preview {
     StatusView()
 }
+
+//.chartForegroundStyleScale([
+//    "todo": .pink,
+//    "wantTodo": .blue
+//    "notdo": .gray
+//])
