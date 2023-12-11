f366c07040feca0253c849129150fa7a6e41b618
Eunice <jes.e.0927@gmail.com>
2023-12-11 16:31:41 +0900

    Refacotr: 불필요한 ZStack 삭제

--- PJ2T12_Again12/PJ2T12_Again12/Views/HomeView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/HomeView.swift
@@ -13,107 +13,105 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                ZStack {
-                    Color(hex: 0xFFFAE1)
-                        .ignoresSafeArea()
-                    ScrollView {
-                        // NavBar
-                        HStack {
-                            Text("2023. 12")
+                Color(hex: 0xFFFAE1)
+                    .ignoresSafeArea()
+                ScrollView {
+                    // NavBar
+                    HStack {
+                        Text("2023. 12")
+                            .foregroundStyle(Color(hex: 0x432D00))
+                            .font(.largeTitle)
+                            .fontWeight(.medium)
+                        Spacer()
+                        NavigationLink {
+                            HistoryView()
+                        } label: {
+                            Image(systemName: "doc.text")
+                                .font(.title)
                                 .foregroundStyle(Color(hex: 0x432D00))
-                                .font(.largeTitle)
-                                .fontWeight(.medium)
-                            Spacer()
-                            NavigationLink {
-                                HistoryView()
-                            } label: {
-                                Image(systemName: "doc.text")
-                                    .font(.title)
-                                    .foregroundStyle(Color(hex: 0x432D00))
-                            }
-                        }
-                        .padding(.bottom, 4)
-                        VStack(spacing: 16) {
-                            // 하고 싶은 일
-                            VStack {
-                                HStack {
-                                    Text("하고 싶으면")
-                                        .bold()
-                                    Spacer()
-                                }
-                                VStack {
-                                    ForEach(toDoList) { todo in
-                                        Button() {
-                                            homeVM.showingAlert = true
-                                        } label: {
-                                            Text("🍞 " + todo.title)
-                                                .modifier(TodoCellModifier(status: todo.status, hexCode: 0xB79800))
-                                        }
-                                    }
-                                    if toDoList.count < 3 {
-                                        Button {
-                                            homeVM.showingModalAlert = true
-                                        } label: {
-                                            Text("새로운 투두를 추가해보세요")
-                                                .padding()
-                                                .foregroundStyle(.black)
-                                                .frame(maxWidth: .infinity, alignment: .center)
-                                                .background(
-                                                    RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                                        .fill(Color.white.opacity(0.6))
-                                                )
-                                                .padding(.top, 32)
-                                        }
-                                    }
-                                }
-                                .padding()
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color(hex: 0xA58B00).opacity(0.32), lineWidth: 2)
-                                )
-                            }
-                            // 해야 하는 일
-                            VStack {
-                                HStack {
-                                    Text("해야 하면")
-                                        .bold()
-                                    Spacer()
-                                }
-                                VStack {
-                                    ForEach(haveToList) { todo in
-                                        Button() {
-                                            homeVM.showingAlert = true
-                                        } label: {
-                                            Text("🍁 " + todo.title)
-                                                .modifier(TodoCellModifier(status: todo.status, hexCode: 0xB76300))
-                                        }
-                                    }
-                                    if haveToList.count < 3 {
-                                        Button {
-                                            homeVM.showingModalAlert = true
-                                        } label: {
-                                            Text("새로운 투두를 추가해보세요")
-                                                .padding()
-                                                .foregroundStyle(.black)
-                                                .frame(maxWidth: .infinity, alignment: .center)
-                                                .background(
-                                                    RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                                        .fill(Color.white.opacity(0.6))
-                                                )
-                                                .padding(.top, 32)
-                                        }
-                                    }
-                                }
-                                .padding()
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color(hex: 0xA58B00).opacity(0.32), lineWidth: 2)
-                                )
-                            }
                         }
                     }
-                    .padding()
+                    .padding(.bottom, 4)
+                    VStack(spacing: 16) {
+                        // 하고 싶은 일
+                        VStack {
+                            HStack {
+                                Text("하고 싶으면")
+                                    .bold()
+                                Spacer()
+                            }
+                            VStack {
+                                ForEach(toDoList) { todo in
+                                    Button() {
+                                        homeVM.showingAlert = true
+                                    } label: {
+                                        Text("🍞 " + todo.title)
+                                            .modifier(TodoCellModifier(status: todo.status, hexCode: 0xB79800))
+                                    }
+                                }
+                                if toDoList.count < 3 {
+                                    Button {
+                                        homeVM.showingModalAlert = true
+                                    } label: {
+                                        Text("새로운 투두를 추가해보세요")
+                                            .padding()
+                                            .foregroundStyle(.black)
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                            .background(
+                                                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                                    .fill(Color.white.opacity(0.6))
+                                            )
+                                            .padding(.top, 32)
+                                    }
+                                }
+                            }
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color(hex: 0xA58B00).opacity(0.32), lineWidth: 2)
+                            )
+                        }
+                        // 해야 하는 일
+                        VStack {
+                            HStack {
+                                Text("해야 하면")
+                                    .bold()
+                                Spacer()
+                            }
+                            VStack {
+                                ForEach(haveToList) { todo in
+                                    Button() {
+                                        homeVM.showingAlert = true
+                                    } label: {
+                                        Text("🍁 " + todo.title)
+                                            .modifier(TodoCellModifier(status: todo.status, hexCode: 0xB76300))
+                                    }
+                                }
+                                if haveToList.count < 3 {
+                                    Button {
+                                        homeVM.showingModalAlert = true
+                                    } label: {
+                                        Text("새로운 투두를 추가해보세요")
+                                            .padding()
+                                            .foregroundStyle(.black)
+                                            .frame(maxWidth: .infinity, alignment: .center)
+                                            .background(
+                                                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                                    .fill(Color.white.opacity(0.6))
+                                            )
+                                            .padding(.top, 32)
+                                    }
+                                }
+                            }
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color(hex: 0xA58B00).opacity(0.32), lineWidth: 2)
+                            )
+                        }
+                    }
                 }
+                .padding()
                 
                 if homeVM.showingModalAlert {
                     HomeModalView(shown: $homeVM.showingModalAlert)
