67481f4fe461b546299227b7eb4b23d53772e1ef
Eunice <jes.e.0927@gmail.com>
2023-12-12 17:40:47 +0900

    Feat: 로그인 하기 전에 보여지는 화면을 구성했다. #46 #47
    화면의 버튼을 누르면 로그인 하시겠습니까 Alert창이 임시로 보여진다.
    추후 API 연결 이후 변경될 예정이다.

--- PJ2T12_Again12/PJ2T12_Again12/Views/SocialView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/SocialView.swift
@@ -8,8 +8,78 @@
 import SwiftUI
 
 struct SocialView: View {
+    @StateObject private var socialVM = SocialViewModel()
+    
     var body: some View {
-        Text("Social View")
+        if socialVM.isLogin {
+            SocialViewIfLogin(socialVM: socialVM)
+        } else {
+            SocialViewIfNotLogin(socialVM: socialVM)
+        }
+    }
+}
+
+struct SocialViewIfNotLogin: View {
+    @ObservedObject var socialVM: SocialViewModel
+    @State private var alertIsPresented: Bool = false
+    
+    var body: some View {
+        VStack {
+            GeometryReader { geo in
+                VStack {
+                    HStack {
+                        Text("친구")
+                            .font(.system(size: 25, weight: .heavy))
+                            .padding(.leading, 20)
+                        
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                    
+                    Text("로그인 해서\n친구들의 투두리를 살펴보세요")
+                        .foregroundStyle(.gray)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 100)
+                    
+                    Button {
+                        alertIsPresented = true
+                    } label: {
+                        Text("카카오 로그인하기")
+                            .foregroundStyle(.white)
+                            .font(.system(.body, weight: .bold))
+                            .frame(width: 173, height: 52)
+                            .background(.orange)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    } //Button
+                    .alert(isPresented: $alertIsPresented) {
+                        let leftButton = Alert.Button.default(Text("로그인")) {
+                            socialVM.isLogin = true
+                        }
+                        
+                        let rightButton = Alert.Button.default(Text("취소")) {
+                            socialVM.isLogin = false
+                        }
+                        
+                        return Alert(title: Text("로그인 하시겠습니까?"), 
+                                     primaryButton: rightButton,
+                                     secondaryButton: leftButton)
+                    } //alert
+                    
+                    Spacer()
+                } //VStack
+                .frame(width: geo.size.width, height: geo.size.height)
+                .background(.yellow)
+            }
+        } //VStack
+    }
+}
+
+struct SocialViewIfLogin: View {
+    @ObservedObject var socialVM: SocialViewModel
+    
+    var body: some View {
+        Text("로그인되었습니다")
     }
 }
 
