3d383081c0436eda2b62fae883777f432689fce2
Eunice <jes.e.0927@gmail.com>
2023-12-12 16:26:47 +0900

    Add: SocialViewModel 파일 생성

---
+++ PJ2T12_Again12/PJ2T12_Again12/ViewModels/SocialViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SocialViewModel.swift
+//  PJ2T12_Again12
+//
+//  Created by Eunsu JEONG on 12/12/23.
+//
+
+import SwiftUI
+
+@MainActor
+class SocialViewModel: ObservableObject {
+    @Published var isLogin = false
+}
