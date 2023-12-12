d37c0f91bb27e79d3df3cf28a84221f79828522f
burst-cannon <qlrmr111@naver.com>
2023-12-12 13:53:52 +0900

    Design: List 목록 세부조절

--- PJ2T12_Again12/PJ2T12_Again12/Views/SettingsView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/SettingsView.swift
@@ -11,42 +11,120 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                Color(hex: 0xFFFAE1)
-                    .ignoresSafeArea()
-                List {
-                    Section(header: Text("알림 설정")) {
-                        TaskRow()
-                        TaskRow()
-                        TaskRow()
-                        TaskRow()
+                NavigationSplitView {
+                    List {
+                        Rectangle() // 로그인 뷰
+                            .frame(width: 400, height: 100)
+                            .foregroundStyle(Color.white)
+                        
+                        Section(header: Text("알림 설정")) {
+                            TotalAlarmRow()
+                            MyAlarmRow()
+                            FriendsAlarmRow()
+                            NightAlarmRow()
+                        }
+                        Section(header: Text("테마")) {
+                            ThemeRow()
+                        }
+                        Section(header: Text("설정")) {
+                            NavigationLink {
+                                DetailView()
+                            } label: {
+                                Text("공지사항")
+                            }
+                            NavigationLink {
+                                DetailView()
+                            } label: {
+                                Text("정보")
+                            }
+                            NavigationLink {
+                                DetailView()
+                            } label: {
+                                Text("문의사항")
+                            }
+                        }
+                        Section(header: Text("계정 관리")) {
+                            NavigationLink {
+                                DetailView()
+                            } label: {
+                                Text("로그아웃")
+                            }
+                            NavigationLink {
+                                DetailView()
+                            } label: {
+                                Text("회원 탈퇴")
+                            }
+                        }
                     }
-                    Section(header: Text("테마")) {
-                        TaskRow()
-                    }
-                    Section(header: Text("설정")) {
-                        TaskRow()
-                        TaskRow()
-                        TaskRow()
-                    }
-                    Section(header: Text("계정 관리")) {
-                        TaskRow()
-                        TaskRow()
-                    }
+                    .listStyle(.grouped)
+                } detail: {
+                    Text("")
                 }
-                .listStyle(.grouped)
-                
             }
             .navigationBarTitle("설정")
         }
     }
 }
 
-struct TaskRow: View {
-  var body: some View {
-      HStack {
-          Text("Test")
-      }
-  }
+struct TotalAlarmRow: View {
+    @State private var totalToggle = true
+    
+    var body: some View {
+        HStack {
+            Toggle(isOn: $totalToggle, label: {
+                Text("전체 알림")
+                Text("전체 알림을 조절할 수 있습니다.")
+            })
+        }
+    }
+}
+
+struct MyAlarmRow: View {
+    @State private var myToggle = true
+    
+    var body: some View {
+        HStack {
+            Toggle(isOn: $myToggle, label: {
+                Text("나의 알림")
+                Text("나의 일정 알림을 조절할 수 있습니다.")
+            })
+        }
+    }
+}
+
+struct FriendsAlarmRow: View {
+    @State private var friendsToggle = true
+    
+    var body: some View {
+        HStack {
+            Toggle(isOn: $friendsToggle, label: {
+                Text("친구 알림")
+                Text("친구들이 보내는 알림을 조절할 수 있습니다.")
+            })
+        }
+    }
+}
+
+struct NightAlarmRow: View {
+    @State private var nightToggle = true
+    
+    var body: some View {
+        HStack {
+            Toggle(isOn: $nightToggle, label: {
+                Text("야간 알림")
+                Text("야간에 오는 알림을 조절할 수 있습니다.")
+            })
+        }
+    }
+}
+
+struct ThemeRow: View {
+    var body: some View {
+        HStack {
+            Text("로그인 하여 각자의 테마를 만들어 보세요.")
+                .foregroundStyle(Color.gray)
+        }
+    }
 }
 
 #Preview {
