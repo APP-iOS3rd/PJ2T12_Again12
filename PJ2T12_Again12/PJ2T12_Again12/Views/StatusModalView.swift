b5a46ef743ea77fa1da933b51fa90974ab82fd38
Im-loki <al4126@naver.com>
2023-12-14 00:34:06 +0900

    Design: navigationView 추가하여 타이틀 변경

--- PJ2T12_Again12/PJ2T12_Again12/Views/StatusModalView.swift
+++ PJ2T12_Again12/PJ2T12_Again12/Views/StatusModalView.swift
@@ -10,16 +10,20 @@ import SwiftUI
 struct StatusModalView: View {
     
     var body: some View {
-        Text("위대한 계획러")
-        Image(systemName: "hare")
-            .font(.system(size: 20))
-            .frame(width: 90, height: 90)
-            .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray.opacity(0.3)))
-        Text("1년동안 모든 계획을 실행한 유저")
+        ZStack {
+            Color(hex: 0xFFFAE1)
+                .ignoresSafeArea()
+            VStack {
+                Text("위대한 계획러")
+                Image(systemName: "hare")
+                    .font(.system(size: 20))
+                    .frame(width: 90, height: 90)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray.opacity(0.3)))
+                Text("1년동안 모든 계획을 실행한 유저")
+            }
+        }
     }
 }
-
-
 struct StatusModalView_Previews: PreviewProvider {
     static var previews: some View {
         StatusView()
